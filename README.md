93c1daf9406565800fb7cb78b20b29d25d6e730b
zjiamzn <80361351+zjiamzn@users.noreply.github.com>
2021-03-09 14:13:46 -0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,6 @@
+# dg..
+Playing with GH Actions and GitLab CI/CD
+
+Maven Hello World.
+
+Test Push commit-x3
---
+++ src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,25 @@
+package com.mycompany.app;
+
+import javax.crypto.Cipher;
+import java.lang.Exception;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+
+    private void foo() throws Exception {
+        bar("DES"); // this should create a warning
+    }
+
+    private void bar(String s) throws Exception
+    {
+        Cipher c = Cipher.getInstance(s);
+    }
+}
