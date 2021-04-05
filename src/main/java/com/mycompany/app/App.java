5bf221f7baa5e7881fc47f5b77ebe5d46f2f27c8
zjiamzn <80361351+zjiamzn@users.noreply.github.com>
2021-04-05 11:55:20 -0700

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -2,6 +2,7 @@ package com.mycompany.app;
 
 import javax.crypto.Cipher;
 import java.lang.Exception;
+import java.security.SecureRandom;
 
 /**
  * Hello world!
@@ -12,6 +13,11 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        System.out.format("No %s",1);
+        if ("1" == "2") {
+            String str = "1" + 2;
+        }
+        SecureRandom sr = SecureRandom.getInstanceStrong();
     }
 
     private void foo() throws Exception {
