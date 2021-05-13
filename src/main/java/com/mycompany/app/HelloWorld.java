9b84b4cc59238b1438c8caf376c5911dfd30f440
Zhengfei Ji <zhengfj@amazon.com>
2021-05-12 17:04:21 -0700

    string equal - 05121704

--- src/main/java/com/mycompany/app/HelloWorld.java
+++ src/main/java/com/mycompany/app/HelloWorld.java
@@ -7,11 +7,10 @@ package com.mycompany.app;
 public class HelloWorld
 {
     private void foobar() throws Exception {
-	if("16" == "18") {}
-	if("18" == "19") {}
 	if("20" == "20") {}
 	if("22" == "22") {}
 	if("26" == "26") {}
 	if("28" == "28") {}
+	if("30" == "30") {}
     }
 }
