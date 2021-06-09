174c2fafe2fbee95950f8a7b5001c098a226456f
zjiamzn <80361351+zjiamzn@users.noreply.github.com>
2021-06-09 14:42:37 -0700

    Update HelloWorld.java

--- src/main/java/com/mycompany/app/HelloWorld.java
+++ src/main/java/com/mycompany/app/HelloWorld.java
@@ -8,7 +8,7 @@ public class HelloWorld
 {
     private void foobar() throws Exception {
 	if("20" == "20") {}
-	if("22" == "22") {}
+	if("8" == "8") {}
 	if("16" == "16") {}
 	if("30" == "30") {}
 	if("32" == "32") {}
